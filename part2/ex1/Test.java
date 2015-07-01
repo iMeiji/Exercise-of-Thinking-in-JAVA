58ca9c7cf605874b560f0b3a8c3e54998b20ea3b
wangyeming <wangyeming@dotide.com>
2015-07-02 00:45:17 +0800

    finish ex1

---
+++ part2/ex1/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public int number;
+    public char name;
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println("number " + test.number);
+        System.out.println("name " + test.name);
+    }
+}
