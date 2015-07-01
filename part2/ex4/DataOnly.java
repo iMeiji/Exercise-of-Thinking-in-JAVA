15f5cf4ef2f8ca16db4f7128c6ee0589cd0785f7
wangyeming <wangyeming@dotide.com>
2015-07-02 00:55:26 +0800

    finish ex4

---
+++ part2/ex4/DataOnly.java
@@ -0,0 +1,12 @@
+public class DataOnly {
+    int i;
+    double d;
+    boolean b;
+
+    public static void main(String[] args) {
+        DataOnly dataOnly = new DataOnly();
+        dataOnly.i = 1;
+        dataOnly.d = 1d;
+        dataOnly.b = true;
+    }
+}
