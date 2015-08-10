8c7bb990042f0e0e672670d9ba9a4d5202eac90e
王烨明 <1398738509@qq.com>
2015-08-10 22:05:12 +0800

    提交3.12

--- javaExercise/part3/ex12/DisplacementOperator.java
+++ javaExercise/part3/ex12/DisplacementOperator.java
@@ -1,15 +1,16 @@
-package ex11;
+package ex12;
 
 /**
  * Created by yeming on 2015/8/10.
  */
 public class DisplacementOperator {
 
-    public static final long NUMBER = 0x10;   //二进制10000
+    public static final long NUMBER = 0x1F;   //二进制11111
 
     public static void main(String[] args) {
         System.out.println(Long.toBinaryString(NUMBER));
-        Long result = NUMBER ;
+        Long result = NUMBER << 1 ;
+        System.out.println(Long.toBinaryString(result));
         while(result != 0) {
             result = result >> 1;
             System.out.println(Long.toBinaryString(result));
