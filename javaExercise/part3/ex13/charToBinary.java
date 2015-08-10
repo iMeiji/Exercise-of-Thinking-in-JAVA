7417a96d0b03d2bcac77820ba39009d66c7cbdb0
王烨明 <1398738509@qq.com>
2015-08-10 22:16:25 +0800

    提交3.13

---
+++ javaExercise/part3/ex13/charToBinary.java
@@ -0,0 +1,14 @@
+package ex13;
+
+/**
+ * Created by yeming on 2015/8/10.
+ */
+public class charToBinary {
+
+    public static void main(String[] args) {
+        char char1 = 'a';
+        char char2 = 'A';
+        System.out.println(Integer.toBinaryString(char1));
+        System.out.println(Integer.toBinaryString(char2));
+    }
+}
