2401ded4ae0140bab2676f4e562945bdde93fda8
王烨明 <1398738509@qq.com>
2015-08-10 21:33:33 +0800

    提交3.9

---
+++ javaExercise/part3/ex9/MaxNumber.java
@@ -0,0 +1,12 @@
+package ex9;
+
+/**
+ * Created by yeming on 2015/8/10.
+ */
+public class MaxNumber {
+
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Double.MAX_VALUE);
+    }
+}
