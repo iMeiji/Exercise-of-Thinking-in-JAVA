72fe26d2c697e2c022bd640476a305f0bc3ef8c9
王烨明 <1398738509@qq.com>
2015-08-10 20:33:35 +0800

    提交3.1

--- javaExercise/part2/ex11/AllTheColorsOffTheRainbow.java
+++ javaExercise/part2/ex11/AllTheColorsOffTheRainbow.java
@@ -1,10 +1,9 @@
 public class AllTheColorsOffTheRainbow {
     int anIntegerRepresentingColors;
-    void changeTheHueOfTheColor(int newHue);
+    void changeTheHueOfTheColor(int newHue) {};
 
     public static void main(String[] args) {
         AllTheColorsOffTheRainbow allTheColorsOffTheRainbow = new AllTheColorsOffTheRainbow();
-        allTheColorsOffTheRainbow.anIntegerRepresentingColors;
         allTheColorsOffTheRainbow.changeTheHueOfTheColor(0);
     }
 }
---
+++ javaExercise/part3/ex1/PrintText.java
@@ -0,0 +1,12 @@
+import java.lang.System;
+
+public class PrintText {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        print("Hello World!");
+    }
+
+    public static void print(String str) {
+        System.out.println("str");
+    }
+}
